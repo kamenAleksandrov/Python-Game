37310734f96b1177d0e3c862a964b7e46dec1aaf
Kamen Aleksandrov <nemak1862000@gmail.com>
2025-04-28 15:45:28 +0300

    managed to generate a second room and connect them

--- main.py
+++ main.py
@@ -3,9 +3,8 @@ import tcod
 
 from engine import Engine
 from entity import Entity
-from game_map import GameMap
 from input_handlers import EventHandler
-
+from procgen import generate_dungeon
 
 def main() -> None:
     screen_width = 80
@@ -24,7 +23,7 @@ def main() -> None:
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), "@", (255, 255, 255))
     entities = {npc, player}
 
-    game_map = GameMap(map_width, map_height)
+    game_map = generate_dungeon(map_width, map_height)
     engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
 
     # this is how we create the map
