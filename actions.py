3f52db8a7ec827f12543eb67ee0f479058b6d91a
Kamen Aleksandrov <nemak1862000@gmail.com>
2025-04-28 12:37:15 +0300

    simplified engine and worked on actions

--- actions.py
+++ actions.py
@@ -1,9 +1,33 @@
+from __future__ import annotations #this speed up load times
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from engine import Engine
+    from entity import Entity
+
 class Action:
-    pass
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise NotImplementedError()
+
 class EscapeAction(Action):
-    pass
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
+
 class MovementAction(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
         self.dx = dx
         self.dy = dy
+
+    def perform(self, engine: Engine, entity: Entity)-> None:
+        destination_x = entity.x + self.dx
+        destination_y = entity.y + self.dy
+
+        if not engine.game_map.in_bounds(destination_x, destination_y):
+            return # Destination is out of bounds
+        if not engine.game_map.tiles["walkable"][destination_x, destination_y]:
+            return # Destination is blocked by a tile.
+        entity.move(self.dx, self.dy)
