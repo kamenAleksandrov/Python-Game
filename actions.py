e7da818b463dc9a85bd3bec3160eb8d7ab1c9d74
Kamen Aleksandrov <nemak1862000@gmail.com>
2025-04-28 20:30:00 +0300

    implemented entity collision and attack action

--- actions.py
+++ actions.py
@@ -1,4 +1,4 @@
-from __future__ import annotations #this speed up load times
+from __future__ import annotations  # this speed up load times
 
 from typing import TYPE_CHECKING
 
@@ -6,29 +6,64 @@ if TYPE_CHECKING:
     from engine import Engine
     from entity import Entity
 
+
 class Action:
 
     # this forces other extenders to implement their own perform
     def perform(self, engine: Engine, entity: Entity) -> None:
         raise NotImplementedError()
 
+
 class EscapeAction(Action):
 
     def perform(self, engine: Engine, entity: Entity) -> None:
         raise SystemExit()
 
-class MovementAction(Action):
+
+class ActionWithDirection(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
+
         self.dx = dx
         self.dy = dy
 
-    def perform(self, engine: Engine, entity: Entity)-> None:
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise NotImplementedError()
+
+
+class BumpAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return MeleeAction(self.dx, self.dy).perform(engine, entity)
+        else:
+            return MovementAction(self.dx, self.dy).perform(engine, entity)
+
+
+class MeleeAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        destination_x = entity.x + self.dx
+        destination_y = entity.y + self.dy
+        target = engine.game_map.get_blocking_entity_at_location(destination_x, destination_y)
+        if not target:
+            return
+
+        print(f"You hit the {target.name}!")
+
+
+class MovementAction(ActionWithDirection):
+
+    def perform(self, engine: Engine, entity: Entity) -> None:
         destination_x = entity.x + self.dx
         destination_y = entity.y + self.dy
 
         if not engine.game_map.in_bounds(destination_x, destination_y):
-            return # Destination is out of bounds
+            return  # Destination is out of bounds
         if not engine.game_map.tiles["walkable"][destination_x, destination_y]:
-            return # Destination is blocked by a tile.
+            return  # Destination is blocked by a tile.
+        if engine.game_map.get_blocking_entity_at_location(destination_x, destination_y):
+            return
+
         entity.move(self.dx, self.dy)
