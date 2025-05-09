aef096e925e927cacb7947e66329b0c3cd9030e6
Kamen Aleksandrov <nemak1862000@gmail.com>
2025-05-09 19:03:39 +0300

    added a message log but cant display it yet
    i can seem to find the problem but i think it may have something to do with its position

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import copy
 import tcod
 
+import color
+
 from engine import Engine
 import entity_factories
 from procgen import generate_dungeon
@@ -11,7 +13,7 @@ def main() -> None:
     screen_height = 50
 
     map_width = 80
-    map_height = 45
+    map_height = 43
 
     room_max_size = 10
     room_min_size = 6
@@ -36,6 +38,10 @@ def main() -> None:
         engine=engine,
     )
     engine.update_fov()
+
+    engine.message_log.add_message(
+        "Hello and welcome, adventurer, to yet another dungeon!", color.welcome_text
+    )
 
     # this is how we create the map
     with tcod.context.new_terminal(
