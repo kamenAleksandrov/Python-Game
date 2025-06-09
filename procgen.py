6c12df845afa2b8867cdd190b51d84de2a7ede6d
Kamen Aleksandrov <nemak1862000@gmail.com>
2025-06-09 18:17:39 +0300

    monsters and items now increase in number as  the player descends

--- procgen.py
+++ procgen.py
@@ -13,6 +13,28 @@ import tile_types
 if TYPE_CHECKING:
     from engine import Engine
 
+max_items_by_floor = {
+    (1,1),
+    (3, 2)
+}
+
+max_monsters_by_floor = {
+    (1,2),
+    (3,2),
+    (5,5),
+}
+
+def get_max_value_for_floor(
+    max_value_by_floor: List[Tuple[int, int]], floor: int
+) -> int:
+    current_value = 0
+
+    for floor_minimum, value in max_value_by_floor:
+        if floor_minimum > floor:
+            break
+        else:
+            current_value = value
+    return current_value
 
 class RectangularRoom:
     def __init__(self, x: int, y: int, width: int, height: int):
@@ -44,10 +66,15 @@ class RectangularRoom:
 
 
 def place_entities(
-    room: RectangularRoom, dungeon: GameMap, maximum_monsters: int, maximum_items: int,
+    room: RectangularRoom, dungeon: GameMap, floor_number: int,
 ) -> None:
-    number_of_monsters = random.randint(0, maximum_monsters)
-    number_of_items = random.randint(0, maximum_items)
+
+    number_of_monsters = random.randint(
+        0, get_max_value_for_floor(max_monsters_by_floor, floor_number)
+    )
+    number_of_items = random.randint(
+        0, get_max_value_for_floor(max_items_by_floor, floor_number)
+    )
 
     for i in range(number_of_monsters):
         x = random.randint(room.x1 + 1, room.x2 - 1)
@@ -102,8 +129,6 @@ def generate_dungeon(
     room_max_size: int,
     map_width: int,
     map_height: int,
-    max_monsters_per_room: int,
-    max_items_per_room: int,
     engine: Engine,
 ) -> GameMap:
     """Generate a new dungeon map."""
@@ -137,7 +162,7 @@ def generate_dungeon(
 
                 center_of_last_room = new_room.center
 
-        place_entities(new_room, dungeon, max_monsters_per_room, max_items_per_room)
+        place_entities(new_room, dungeon, engine.game_world.current_floor)
 
         dungeon.tiles[center_of_last_room] = tile_types.stairs_down
         dungeon.stairsdown_location = center_of_last_room
